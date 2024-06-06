b8d2db79b41e521995835ea3370312022393eed7
John ray Judaya <jrj@newmediastaff.com>
2024-06-06 16:25:45 +0800

    chore: Update root component to start with nothing.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,20 +2,19 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+const rootElement = document.getElementById('app');
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+if (rootElement) {
+    createRoot(rootElement).render(<App />);
+}
+
+function App() {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    );
+}

eb370241234c01972f3832da989e78c7167818ec
Judaemon <judayajohnray@gmail.com>
2024-06-20 14:28:16 +0800

    refactor: Update SampleComponent to use Zustand for state management

--- resources/js/Components/SampleComponent.tsx
+++ resources/js/Components/SampleComponent.tsx
@@ -1,15 +1,16 @@
-import { useCounter } from '@/Hooks/sampleHook';
+import { useBearStore } from '@/Hooks/sampleHook';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const Sample = () => {
-  const [state, actions] = useCounter();
-  console.log('testing', state.count);
+  const bears = useBearStore((state) => state.bears);
+  const increasePopulation = useBearStore((state) => state.increasePopulation);
+  console.log('bears', bears);
 
   return (
     <div>
-      <p>Count: {state.count}</p>
-      <button onClick={() => actions.increment()}>Increment</button>
+      <p>Bear Count: {bears}</p>
+      <button onClick={() => increasePopulation()}>Increment Population</button>
     </div>
   );
 };
@@ -37,6 +38,29 @@ export const SampleUsers = () => {
           </li>
         ))}
       </ul>
+    </div>
+  );
+};
+
+export const SampleAPICall = () => {
+  // get request to /test
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['sample'],
+    queryFn: () =>
+      axios.get('/test123').then((res) => {
+        console.log('res', res);
+        return res.data;
+      }),
+  });
+
+  if (isPending) return <div>Loading...</div>;
+  if (error) return <div>An error occurred: {error.message}</div>;
+
+  return (
+    <div>
+      <h1>Data</h1>
+      {isFetching ? <div>Updating...</div> : null}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 };

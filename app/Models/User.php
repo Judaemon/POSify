624c575f34d083ed81c45faf54627c212cdbb45a
Judaemon <judayajohnray@gmail.com>
2024-07-01 17:37:56 +0800

    refactor: Add spatie/laravel-model-status package for managing model statuses

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\ModelStatus\HasStatuses;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles, HasStatuses;
+
+    const STATUS_ACTIVE = "active";
+    const STATUS_INACTIVE = "inactive";
+    const STATUS_DELETED = "deleted";
 
     /**
      * The attributes that are mass assignable.

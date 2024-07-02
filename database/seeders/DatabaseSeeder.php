59802b49edbbb9410e7dd589de9b4c89bc7d24d6
Judaemon <judayajohnray@gmail.com>
2024-07-02 12:55:51 +0800

    refactor: Update DatabaseSeeder to create a user with a specific team and set their statuses

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Enums\TeamStatus;
+use App\Enums\UserStatus;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -9,17 +11,23 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-  /**
-   * Seed the application's database.
-   */
-  public function run(): void
-  {
-    // User::factory(10)->create();
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void
+	{
+		// User::factory(10)->create();
 
-    User::factory()->create([
-      'name' => 'Test User',
-      'email' => 'test@gmail.com',
-      'password'=> Hash::make('123123123'),
-    ])->setStatus(User::STATUS_ACTIVE, "User creation");
-  }
+		$user = User::factory()->create([
+			'name' => 'Test User',
+			'email' => 'test@gmail.com',
+			'password' => Hash::make('123123123'),
+		]);
+
+		$user->setStatus(UserStatus::ACTIVE->value, "User creation");
+		$user->teams()->create([
+			'name' => "Test's Team",
+			'description' => "This is your personal team. You can create more teams and invite other users to collaborate.",
+		])->setStatus(TeamStatus::ACTIVE->value, "User specific team creation");
+	}
 }
